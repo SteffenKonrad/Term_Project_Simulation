f1d26249712860a03de00c6d07ad14461a2ea8e8
Flo Kirch <kirch.florian@th-bingen.de>
2017-08-09 19:09:03 +0200

    erster Entwurf habe noch so meine Problemchen

---
+++ src/main/kotlin/traffic_simulation2/cars.kt
@@ -0,0 +1,11 @@
+package traffic_simulation2
+
+
+data  class cars ( val name : String, val drives : Boolean)
+{
+
+    }
+
+/**
+ * Created by Florian Kirch on 08.08.2017.
+ */
---
+++ src/main/kotlin/traffic_simulation2/network.kt
@@ -0,0 +1,11 @@
+package traffic_simulation2
+
+ data class network (var capacity: Int)
+{
+    var capacity = 0
+
+}
+
+/**
+ * Created by Florian Kirch on 09.08.2017.
+ */
