b0e1723b6acb0a9c62935987abc99a5724612890
Hendrik Hoffmann <hoffmann.hendrik@web.de>
2017-08-10 13:54:27 +0200

    .kt

--- src/main/kotlin/traffic_simulation2/cars.kt
+++ src/main/kotlin/traffic_simulation2/cars.kt
@@ -1,11 +1,12 @@
 package traffic_simulation2
 
 
-data  class cars ( val name : String, val drives : Boolean)
+data  class Cars ( var carName : String, var driving : Boolean)
 {
 
     }
 
 /**
  * Created by Florian Kirch on 08.08.2017.
+ * edit by Hendrik Hoffmann on 10.08.2017.
  */
