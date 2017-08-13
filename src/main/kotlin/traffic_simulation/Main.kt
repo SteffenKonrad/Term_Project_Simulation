38ed54fbc9ba3aad70a8361199b9163d4b0bab85
Steffen Konrad <steffen.konrad@th-bingen.de>
2017-08-13 15:05:14 +0200

    cosmetic code + add comments

--- src/main/kotlin/traffic_simulation/Main.kt
+++ src/main/kotlin/traffic_simulation/Main.kt
@@ -19,10 +19,10 @@ fun readCsvFile(fileName: String): MutableList<Car> {
 // this function is used to parse and read the CSV given data
 
     val settings = CsvParserSettings()
-    val csvParser = CsvParser(settings)                       // creates a CSV parser
-    val reader = AccessCSV().getReader("/" +fileName)
+    val csvParser = CsvParser(settings)                      // creates a CSV parser
+    val reader = AccessCSV().getReader("/" + fileName)
     settings.isHeaderExtractionEnabled = true
-    settings.format.setLineSeparator("\n")                    // line separator can be changed
+    settings.format.setLineSeparator("\n")                   // line separator can be changed
 
 // here we search the rows of the given csv list for all cars and assign the carName and driving variable
 
@@ -30,27 +30,27 @@ fun readCsvFile(fileName: String): MutableList<Car> {
     val allRows: MutableList<Record> = csvParser.parseAllRecords(reader)
 
     for (car in allRows) {
-        val firstValue: String = car.values.get(0)           //first value of csv assigned to the cars name
+        val firstValue: String = car.values.get(0)           // first value of csv assigned to the cars name
         val secondValue: String = car.values.get(1)          // second value of the csv assigned to driving status
         val drivingStatus: Boolean = secondValue.toBoolean()
 
-        listOfCars.add(Car(carname = firstValue, driving = drivingStatus))
+        listOfCars.add(Car(carname = firstValue, driving = drivingStatus))    // list of cars in which include the car name and if it drives
     }
     return listOfCars
 }
-        // function to parse the resulting kotlin files as csv
+
+// function to parse the resulting kotlin files as csv
 fun outputToCsv(results: List<Car>, fileName: String = "results.csv") {
 
-    val settings = CsvWriterSettings()
+    val settings = CsvWriterSettings()                          // creates a CSV writer
     val writer = AccessCSV().getWriter(fileName)
     val csvWriter = CsvWriter(writer, CsvWriterSettings())
     csvWriter.writeHeaders("Carname", "Driving Status")
-    settings.format.setLineSeparator("\n")
+    settings.format.setLineSeparator("\n")                      // line separator can be changed
 
-      //val carname = "Carname:"
     val checkdelays = "is delayed?"
-    val carRows: MutableList<Array<Any>> = mutableListOf()
-    val row: Array<Any> = arrayOf(" ",checkdelays)
+    val carRows: MutableList<Array<Any>> = mutableListOf()      // array in which the individual carriages stand.
+    val row: Array<Any> = arrayOf(" ", checkdelays)
     carRows.add(row)
 
     for (result in results) {
@@ -62,18 +62,6 @@ fun outputToCsv(results: List<Car>, fileName: String = "results.csv") {
     csvWriter.writeRowsAndClose(carRows)
 }
 
-/** here needs to be added, what results (drives vs delay) we want to parse as csv
- * might be solved with a for-loop, adapted from energy model:
-val carRows: MutableList<Array<Any>> = mutableListOf()
-for (car in listOfCars ) {
-val name = car.name
-val checkDelay= car.driving
-val row: Array<Any> = arrayOf(name, checkDelay)
-carRows.add(row)
-}
-
-csvWriter.writeRowsAndClose(carRows)
- */
 fun buildCSV() {
     val street: Network = Network(capacity = 2)
     val carsCSV: List<Car> = readCsvFile(fileName = "readingdemand.csv")
@@ -82,7 +70,7 @@ fun buildCSV() {
 
 }
 
-
+// Scenario Function in which a list is created and compared from the entered variables
 fun scenario() {
 
     val Car1: Car = Car(carname = "Elenor", driving = true)
@@ -102,9 +90,9 @@ fun scenario() {
 
     val street: Network = Network(capacity = 2)
 
-    for (car in street.testScenario(scenarioList)) {
+    for (car in street.testScenario(scenarioList)) {            // Capacity is compared with demand
         if (car.driving) {
-            println("\"${car.carname}\" wants to drive and is delayed:\"${car.gettingDelayed}\"")
+            println("\"${car.carname}\" wants to drive and is delayed:\"${car.gettingDelayed}\"")         // Output whether a car is delayed or not
 
         } else {
             println("\"${car.carname}\" does not want to drive and is delayed: \"${car.gettingDelayed}\"")
--- src/main/kotlin/traffic_simulation/Network.kt
+++ src/main/kotlin/traffic_simulation/Network.kt
@@ -13,14 +13,15 @@ class Network(var capacity: Int) {
         return requiredcapacity
     }
 
-
-    fun comparison(requirement: Int): Boolean {     // what is requirement and where is this set?
+    // function compares the capacity and the demand
+    fun comparison(requirement: Int): Boolean {
         if (capacity <= requirement) {
             return true
         }
         return false
     }
 
+    // function asks whether the capacity is exhausted and whether or not a delay occurs
     fun testScenario(carList: List<Car>): List<Car> {
         val requirement: Int = neededCapacity(carList)
         val compare: Boolean = comparison(requirement)
@@ -34,6 +35,5 @@ class Network(var capacity: Int) {
             }
         }
         return carList
-
     }
 }
